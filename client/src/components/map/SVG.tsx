ec4aedf2b74a2f7a053f2e976259c518ab0fd81f
alexander-braun <alexanderbraun@freenet.de>
2020-08-01 13:30:21 +0200

    reworked the svg to display curves instead of straight lines

--- client/src/components/map/SVG.tsx
+++ client/src/components/map/SVG.tsx
@@ -12,29 +12,54 @@ function SVG(props: SVG): JSX.Element | null {
   const strokeWidth = !props.center ? '2.5' : '4';
   const insetSvgBy = 5;
 
+  const moveToX =
+    props.childRect.x > props.parentRect.x && !props.center
+      ? props.parentRect.x + props.parentRect.width - insetSvgBy
+      : props.center
+      ? props.parentRect.x + props.parentRect.width / 2
+      : props.parentRect.x + insetSvgBy;
+
+  const moveToY =
+    props.parentRect.y + props.parentRect.height / 2 + scrollHeight;
+
+  const curveX =
+    props.childRect.x < props.parentRect.x && !props.center
+      ? props.childRect.x + props.childRect.width
+      : props.center
+      ? props.childRect.x + props.childRect.width / 2
+      : props.childRect.x;
+
+  const curveY = props.childRect.y + props.childRect.height / 2 + scrollHeight;
+
+  const curveX1 =
+    props.childRect.x > props.parentRect.x
+      ? moveToX + 20
+      : props.center
+      ? moveToX - 40
+      : moveToX - 20;
+
+  const curveX2 =
+    props.childRect.x > props.parentRect.x
+      ? moveToX - 50
+      : props.center
+      ? moveToX + 40
+      : moveToX + 50;
+
+  const curveY1 = moveToY;
+
+  const curveY2 = moveToY;
+
   return (
-    <line
-      x1={
-        props.childRect.x > props.parentRect.x && !props.center
-          ? props.parentRect.x + props.parentRect.width - insetSvgBy
-          : props.center
-          ? props.parentRect.x + props.parentRect.width / 2
-          : props.parentRect.x + insetSvgBy
-      }
-      y1={props.parentRect.y + props.parentRect.height / 2 + scrollHeight}
-      x2={
-        props.childRect.x < props.parentRect.x && !props.center
-          ? props.childRect.x + props.childRect.width
-          : props.center
-          ? props.childRect.x + props.childRect.width / 2
-          : props.childRect.x
-      }
-      y2={props.childRect.y + props.childRect.height / 2 + scrollHeight}
-      stroke='#2b78e4'
-      strokeDasharray={strokeDasharray}
-      strokeLinecap='round'
-      strokeWidth={strokeWidth}
-    />
+    <>
+      <path
+        d={`M ${moveToX} ${moveToY} C ${curveX1} ${curveY1}, ${curveX2} ${curveY2}, ${curveX} ${curveY}`}
+        stroke='#2b78e4'
+        fill='transparent'
+        strokeDasharray={strokeDasharray}
+        strokeLinecap='round'
+        strokeWidth={strokeWidth}
+      />
+    </>
   );
 }
 
