da0e72c4867480abb34b7af9c4192f5ab9d56fe0
alexander-braun <alexanderbraun@freenet.de>
2020-08-08 23:20:06 +0200

    prebuild 7

---
+++ client/build/precache-manifest.6eaf21cc69c819b42d3c14b1141274ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abf33c6102f76204e69793386ea0a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07c4d1e3aa95c0a06f98",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "dacdd954ca35571eb72f",
+    "url": "/static/css/main.256b1b92.chunk.css"
+  },
+  {
+    "revision": "07c4d1e3aa95c0a06f98",
+    "url": "/static/js/2.d189428f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d189428f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dacdd954ca35571eb72f",
+    "url": "/static/js/main.fd844c6d.chunk.js"
+  },
+  {
+    "revision": "19456d371fe63ce61c01",
+    "url": "/static/js/runtime-main.009aece9.js"
+  },
+  {
+    "revision": "2d6b200049dc4d729499aebd65bd1f3f",
+    "url": "/static/media/Quadrit.2d6b2000.svg"
+  },
+  {
+    "revision": "3108f7f4f171cdacb0ff68ac6ac040d8",
+    "url": "/static/media/Quadrit.3108f7f4.woff2"
+  },
+  {
+    "revision": "7b1530a4519fb303b82e04e48941e720",
+    "url": "/static/media/Quadrit.7b1530a4.woff"
+  },
+  {
+    "revision": "b4878e03f67a10e117d6a92e8c7769f2",
+    "url": "/static/media/Quadrit.b4878e03.ttf"
+  },
+  {
+    "revision": "df2a34bdef4b3d12deccf6c87a144d0b",
+    "url": "/static/media/Quadrit.df2a34bd.eot"
+  }
+]);
