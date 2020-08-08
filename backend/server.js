516d120a67a5221b9df0ec627c1c8793e15c2b46
alexander-braun <alexanderbraun@freenet.de>
2020-08-08 23:35:55 +0200

    prebuild 9

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/presets', require('./routes/api/presets'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/../client', 'build', 'index.html'));
   });
 }
 
