f3e2cb6dd4c95c9fd39bf3a412b3ce8070bfeedf
alexander-braun <alexanderbraun@freenet.de>
2020-08-12 14:50:52 +0200

    Trying different nodemailer settings for gmail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,6 +6,9 @@ dotenv.config();
 const sendEmail = async (options) => {
   let transporter = nodemailer.createTransport({
     service: process.env.SMTP_HOST,
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
